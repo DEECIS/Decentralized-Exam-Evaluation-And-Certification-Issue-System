fd0bfa12aa24af75130b41029f4458ca242593da
tim-hub <tim_w@outlook.com>
2018-07-24 22:20:57 +1200

    add alert of undefined web3

--- src/components/base.js
+++ src/components/base.js
@@ -17,6 +17,8 @@ export default{
       web3 = new Web3(web3.currentProvider)
     } else {
       console.warn("No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask")
+
+      alert("No Web3, did you install metamask?");
       // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
       web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"))
     }
