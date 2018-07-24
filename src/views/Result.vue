7c40e9b97cee59d6bcf57bb978a3d69de7178c3e
tim-hub <tim_w@outlook.com>
2018-07-24 16:29:37 +1200

    update vue add a reward image

--- src/views/Result.vue
+++ src/views/Result.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <MyHeader/>
-
-    this is result page
+  <div>
+    <img src="@/assets/reward.jpg"/>
+  </div>
   </div>
 </template>
 <script>
@@ -10,11 +11,33 @@
 // @ is an alias to /src
 
 import MyHeader from '@/components/MyHeader.vue'
+import base from "@/components/base.js"
 
 export default {
   name: 'home',
   components: {
     MyHeader
+  },
+  data() {
+    return {
+      id:0
+    }
+  },
+  created(){
+    console.log("created "+ this.$route.params.id);
+
+    // init web3
+    base.initWeb3();
+    // set the country list
+    this.DEE = base.CL;
+
+    this.id = parseInt(this.$route.params.id);
+
+    if (this.id == 0){
+
+    }else{
+
+    }
   }
 }
 </script>
