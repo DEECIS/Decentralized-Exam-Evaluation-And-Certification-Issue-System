7f1af40dd9a212ac52e77bfb38a2318de179a57d
tim-hub <tim_w@outlook.com>
2018-07-26 22:22:34 +1200

    get a basic result

--- src/views/Result.vue
+++ src/views/Result.vue
@@ -22,20 +22,31 @@
     <div   v-else>
 
       <div class="result column">
+        {{getScore()}}
         <img  src="@/assets/reward.jpg"/>
         <div class="top-left">
-          To the owner of account:
+          <strong>To the owner of account:</strong>
           <br/>
           {{base.accounts[0]}}
         </div>
-        {{getScore()}}
-        {{date.getDate()}}-{{date.getMonth()}}-{{date.getFullYear()}}
+
+        <div class="in-center">
+          <p>
+            The score of
+            <router-link :to="{ name: 'quiz', params: { id: 0 }}">
+              {{Exam.name}}
+            </router-link> is:
+          </p>
+          <p class="is-size-1">
+            {{result}}
+          </p>
+        </div>
+
+        <div class="bottom-right">
+          Issued by <span class="is-size-4">DEE</span>
+          <p>{{date.getDate()}}-{{date.getMonth()}}-{{date.getFullYear()}}</p>
+        </div>
       </div>
-
-
-
-
-
     </div>
   </div>
 </template>
@@ -43,16 +54,19 @@
 
 // @ is an alias to /src
 import MyHeader from '@/components/MyHeader.vue'
+import Exam from '@/views/Exam.vue'
 import base from "@/components/base.js"
 
 export default {
-  name: 'home',
+  name: 'certification',
   components: {
     MyHeader
+
   },
   data() {
     return {
       base,
+      Exam,
       img: "{ background-image : url("+ require('@/assets/reward.jpg') + ")}",
       id:0,
       address: 0,
@@ -102,15 +116,29 @@ export default {
 .result{
   margin-left: 175px;
   position: relative;
-  text-align: left;
+  text-align: center;
   height: 608px;
   width: 850px;
 
 }
 .top-left{
   position: absolute;
+  text-align: left;
   top: 220px;
   left: 80px;
+}
+.in-center{
+  position: absolute;
+  text-align: right;
+  top: 350px;
+  left: 320px;
+}
+
+.bottom-right{
+  position: absolute;
+  text-align: right;
+  bottom: 90px;
+  right: 120px;
 }
 
 </style>
